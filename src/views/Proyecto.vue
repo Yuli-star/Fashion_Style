d01a98554866b44e8c1d45383d75cc7421ebaf45
mcmenamc <64565280+mcmenamc@users.noreply.github.com>
2021-11-18 21:45:32 -0600

    actulizacion de proyecto

---
+++ src/views/Proyecto.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+      <h1>
+          {{mensaje}}
+      </h1>
+  </div>
+</template>
+
+<script>
+export default {
+name:"poryecto",
+data(){
+    return{
+        mensaje:"hola yuli",
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
