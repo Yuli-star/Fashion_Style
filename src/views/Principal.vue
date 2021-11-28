cd5b4637b0e1e20455c9fe1967af2fb417dd7ce3
Alo1920 <alondritalopez2018@gmail.com>
2021-11-27 21:47:27 -0600

    Se agrego una nueva secccion

--- src/views/Principal.vue
+++ src/views/Principal.vue
@@ -44,6 +44,60 @@
         </div>
     </section>
 
+    <section>
+        <head>
+            <meta charset="UFF-8">
+            <meta name="viewport" content="width=device.width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+            <title> Carrusel</title>   
+        </head>
+        <body>
+            <div class="contenedor">
+                <div class="item1"></div>
+                <div class="item2"></div>
+                <div class="item3"></div>
+                <div class="item4"></div>
+                <div class="item5"></div>
+            </div>
+            <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                       <img :src="require('../assets/imagenes/invierno.jpg')" class="d-block w-100" alt="imageninvierno">
+                    </div>
+                    <div class="carousel-item">
+                        <img :src="require('../assets/imagenes/formal.jpg')" class="d-block w-100" alt="imagenformal">
+                    </div>
+                    <div class="carousel-item">
+                        <img :src="require('../assets/imagenes/primavera.jpg')" class="d-block w-100" alt="imagenprimavera">
+                    </div>
+                    <div class="carousel-item">
+                        <img :src="require('../assets/imagenes/deportiva.jpg')" class="d-block w-100" alt="imagendeportiva">
+                    </div>
+                    <div class="carousel-item">
+                        <img :src="require('../assets/imagenes/otono.jpg')" class="d-block w-100" alt="imagenotono">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+        </body>
+    </section>
+
     <section id="testimonial">
         <div class="container">
             <h4>Aprovecha la oportunidad y compra las marcas que te gustan</h4>
